8d08fdf52b2237ab04565e1fa3ab4c251368669c
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-26 19:18:35 +0100

    wrapped the bot in express

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import TelegramBot from 'node-telegram-bot-api';
+import { configureBot } from './controllers/botController';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT ?? 3000;
+const app = express();
+
+const token = process.env.TOKEN as string; // gotten from "BotFather" on Telegram
+const bot = new TelegramBot(token, { polling: true });
+configureBot(bot);
+
+app.get('/', (req, res) => {
+    res.send('Ndeewo');
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
